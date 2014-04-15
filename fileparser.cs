86bcc34b9342fc4e9ad76ba9160472fe2059a664
Oleg Shilo <oshilo@gmail.com>
2014-04-15 14:06:50 +1000

    Updated source code licenses

--- fileparser.cs
+++ fileparser.cs
@@ -14,33 +14,24 @@
 // of the namespacs and assemblies used by the script code.
 //
 // Written by Oleg Shilo (oshilo@gmail.com)
-// Copyright (c) 2004-2013. All rights reserved.
-//
-// Redistribution and use of this code WITHOUT MODIFICATIONS are permitted provided that
-// the following conditions are met:
-// 1. Redistributions must retain the above copyright notice, this list of conditions
-//  and the following disclaimer.
-// 2. Neither the name of an author nor the names of the contributors may be used
-//	to endorse or promote products derived from this software without specific
-//	prior written permission.
-//
-// Redistribution and use of this code WITH MODIFICATIONS are permitted provided that all
-// above conditions are met and software is not used or sold for profit.
-//
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
-// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-//	Caution: Bugs are expected!
+//----------------------------------------------
+// The MIT License (MIT)
+// Copyright (c) 2014 Oleg Shilo
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+// and associated documentation files (the "Software"), to deal in the Software without restriction, 
+// including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+// subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial 
+// portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT 
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //----------------------------------------------
 
 #endregion Licence...
@@ -191,7 +182,7 @@ namespace CSScriptLibrary
 
         public string fileNameImported = "";
         public ParsingParams prams = null;
-        
+
         public string FileToCompile
         {
             get { return imported ? fileNameImported : fileName; }
@@ -336,7 +327,7 @@ namespace CSScriptLibrary
         {
             string retval = ResolveFile(file, extraDirs, "");
             if (retval == "")
-                retval = ResolveFile(file, extraDirs, ".cs"); 
+                retval = ResolveFile(file, extraDirs, ".cs");
             if (retval == "")
                 retval = ResolveFile(file, extraDirs, ".csl"); //script link file
 
@@ -845,7 +836,7 @@ namespace CSScriptLibrary
         private void PushItem(ArrayList collection, string item)
 #else
         private void PushItem(List<string> collection, string item)
-#endif   
+#endif
         {
             if (collection.Count > 1)
                 collection.Sort();
@@ -857,7 +848,7 @@ namespace CSScriptLibrary
         {
             PushItem(referencedNamespaces, nameSpace);
         }
-       
+
         private void PushPrecompiler(string file)
         {
             PushItem(precompilers, file);
