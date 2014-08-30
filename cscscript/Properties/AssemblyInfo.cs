55a0c192652110084ec0119c50fca47531c45d48
oleg_s <oshilO@gmail.com>
2014-08-30 22:02:16 +1000

    Release v3.8.6

--- cscscript/Properties/AssemblyInfo.cs
+++ cscscript/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.8.5.0")]
-[assembly: AssemblyFileVersion("3.8.5.0")]
+[assembly: AssemblyVersion("3.8.6.0")]
+[assembly: AssemblyFileVersion("3.8.6.0")]
